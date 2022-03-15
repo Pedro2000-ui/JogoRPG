6126ae588debd3066125a0a70219196a9a6005c0
Pedro <pholiveirabrito@hotmail.com>
2022-03-15 18:34:15 -0300

    Atualização de Código

--- JogoRPG/JogoRPG/Mago.cs
+++ JogoRPG/JogoRPG/Mago.cs
@@ -19,12 +19,25 @@ namespace JogoRPG
                 {
                     this.status.Vida = this.status.Vida + Defesa - AtaqueRecebido;
                     Console.WriteLine("Vida Atual do Mago " + this.nome + " : " + this.status.Vida);
+                    if(this.status.Mana > 0 && this.status.Vida > 0) //Verifica o novo status da vida, para não tentar se curar caso estiver morto, visto que a primeira verificação de vida se dá a vida anterior
+                    {
+                        this.cura();
+                    }
                     return;
                 }
                 Console.WriteLine("O Mago " + this.nome + " apresentou uma defesa maior que o ataque de seu inimigo e por isso não perdeu vida");
                 return;
             }
             Console.WriteLine("Mago " + this.nome + " está morto e não pode mais ser atacado nem Atacar");
+        }
+
+        public void cura()
+        {
+            Console.WriteLine("O Mago " + this.nome + " está se curando....");
+            this.status.Mana = this.status.Mana - 10;
+            this.status.Vida = this.status.Vida + 5;
+            Console.WriteLine("Vida atual: " + this.status.Vida);
+            Console.WriteLine("Mana atual: " + this.status.Mana);
         }
     }
 }
--- JogoRPG/JogoRPG/Program.cs
+++ JogoRPG/JogoRPG/Program.cs
@@ -10,6 +10,9 @@ namespace JogoRPG
     {
         static void Main(string[] args)
         {
+            
+                                        //Criação de Heróis//
+            
             //Criação de um Guerreiro
             Guerreiro thor = new Guerreiro();
             Status statusThor = new Status();
@@ -38,6 +41,21 @@ namespace JogoRPG
             arqueiro.nome = "Clint";
             arqueiro.status = statusArqueiro;
 
+            //Criação de um Mago
+            Mago mago = new Mago();
+            Status statusMago = new Status();
+            statusMago.Forca = 40;
+            statusMago.Defesa = 20;
+            statusMago.Agilidade = 10;
+            statusMago.Sorte = 20;
+            statusMago.Vida = 160;
+            statusMago.Mana = 45;
+            mago.Ataque = statusMago.Forca + statusMago.Agilidade;
+            mago.Defesa = statusMago.Defesa + statusMago.Agilidade;
+            mago.nome = "Ferdinando";
+            mago.status = statusMago;
+
+                                        //Criação de Monstros//
 
             //Criação de um Goblin
             Globin evolut = new Globin();
@@ -52,15 +70,62 @@ namespace JogoRPG
             evolut.Defesa = statusEvolut.Defesa + statusEvolut.Agilidade;
             evolut.nome = "Fabianski";
             evolut.status = statusEvolut;
-            
 
-            //Ataques e Defesas
-            for (int i = 0; i < 10; i++)
-            {
-                arqueiro.recebeAtaque(thor.atacar(thor.Ataque, thor.status.Sorte), arqueiro.defender(arqueiro.Defesa, arqueiro.status.Sorte));
-                thor.recebeAtaque(arqueiro.atacar(arqueiro.Ataque, arqueiro.status.Sorte), thor.defender(thor.Defesa, thor.status.Sorte));
-            }
+            Aranha aranha = new Aranha();   
+            Status statusAranha = new Status();
+            statusAranha.Forca = 20;
+            statusAranha.Defesa = 5;
+            statusAranha.Agilidade = 35;
+            statusAranha.Sorte = 10;
+            statusAranha.Vida = 40;
+            statusAranha.Mana = 0;
+            aranha.Ataque = statusAranha.Forca + statusAranha.Agilidade;
+            aranha.Defesa = statusAranha.Defesa + statusAranha.Agilidade;
+            aranha.nome = "Peter Parker";
+            aranha.status = statusAranha;
+
+            Dragao dragao = new Dragao();
+            Status statusDragao = new Status();
+            statusDragao.Forca = 50;
+            statusDragao.Defesa = 10;
+            statusDragao.Agilidade = 25;
+            statusDragao.Sorte = 10;
+            statusDragao.Vida = 80;
+            statusDragao.Mana = 0;
+            dragao.Ataque = statusDragao.Forca + statusDragao.Agilidade;
+            dragao.Defesa = statusDragao.Defesa + statusDragao.Agilidade;
+            dragao.nome = "Banguela";
+            dragao.status = statusDragao;
+
+
+                                        //Batalhas
+                                        //Guerreiro vs Goblin
+            Console.WriteLine("Batalha 1:");
             
+            while (thor.status.Vida > 0 && evolut.status.Vida > 0)
+            {
+                evolut.recebeAtaque(thor.atacar(thor.Ataque, thor.status.Sorte), evolut.defender(evolut.Defesa, evolut.status.Sorte));
+                thor.recebeAtaque(evolut.atacar(evolut.Ataque, evolut.status.Sorte), thor.defender(thor.Defesa, thor.status.Sorte));
+            }
+            Console.WriteLine();
+                                        //Arqueiro vs Aranha
+            Console.WriteLine("Batalha 2:");
+            
+            while (arqueiro.status.Vida > 0 && aranha.status.Vida > 0)
+            {
+                aranha.recebeAtaque(arqueiro.atacar(arqueiro.Ataque, arqueiro.status.Sorte), aranha.defender(aranha.Defesa, aranha.status.Sorte));
+                arqueiro.recebeAtaque(aranha.atacar(aranha.Ataque, aranha.status.Sorte), arqueiro.defender(arqueiro.Defesa, arqueiro.status.Sorte));
+            }
+            Console.WriteLine();
+                                        //Mago vs Dragão
+            Console.WriteLine("Batalha 3:");
+            
+            while (mago.status.Vida > 0 && dragao.status.Vida > 0)
+            {
+                mago.recebeAtaque(dragao.atacar(dragao.Ataque, dragao.status.Sorte), mago.defender(mago.Defesa, mago.status.Sorte));
+                dragao.recebeAtaque(mago.atacar(mago.Ataque, mago.status.Sorte), dragao.defender(dragao.Defesa, dragao.status.Sorte));
+            }
+
         }
     }
 }
